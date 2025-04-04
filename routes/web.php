c3072dfd7a01785835c3d7526b080aeb22ccbcc0
Oliver Yosi <yosi.oliver@qualysoft-coe.com>
2025-04-04 14:37:54 +0700

    add google map api

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\FormController;
 use App\Http\Controllers\MailController;
+use App\Http\Controllers\MapsController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\WhatsappController;
 use Illuminate\Support\Facades\Route;
@@ -13,7 +14,10 @@ Route::get('/', function () {
 Route::get('test-send-mail', [MailController::class, 'index']);
 Route::get('form', [FormController::class, 'index']);
 Route::post('form', [FormController::class, 'store'])->name('storeForm');
+
 Route::get('payment', [PaymentController::class, 'show'])->name('showPayment');
 Route::get('payment-success/{transactionId}', [PaymentController::class, 'showPaymentSuccess'])->name('showPaymentSuccess');
 
 Route::get('send-wa', [WhatsappController::class, 'sendWa'])->name('sendWa');
+
+Route::get('maps', [MapsController::class, 'show'])->name('show');
